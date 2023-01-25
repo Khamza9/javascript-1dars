3327013fa165480d0d134c39dbf33fa508f3e7da
khamza9 <khamzarizaev.@gmail.com>
2023-01-26 02:41:00 +0900

    first commit

--- javascript1/javascrip.js
+++ javascript1/javascrip.js
@@ -1,1 +1,33 @@
-alert('Salom dunyo')
+//                    Namber 
+let n = 1             // nomberlar 15 ta katakgacha uzgarmaydi 
+let k = 0.1           // Float nomberlarni maxsimal soni 17 ta
+let b = 2**53-1       // shu raqamdan katalari BigInt 
+console.log(b)
+//                    string
+//    \" "\   \' '\   \` `\  string  bilgilari
+let q = "bu qush tirnoq." 
+let w = 'bu bir tirnoq.'
+let e = `bu back tag.`
+console.log(q ,w,e)
+//                    Boolean
+//mantiqiy qiymatlarni ifodalaydi 
+let tr = true
+let fa = false
+console.log(tr , fa )
+//                    Undefaind
+// bu bush qiymatlarni ifodalaydi
+let undef ;
+let un = undefined 
+console.log(undef , un )
+//                    null ---?????????????
+
+//                    symbol ---???????????
+
+//                    Object Refernce type
+// bu uzida har xil malumotlarni saqlaydi.
+const person = {
+  firstName: "khamza",
+  lastName: "rizaev",
+  age: 27,
+}
+console.log(person)
